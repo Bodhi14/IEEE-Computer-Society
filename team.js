18b690041c25d3b3e1afd203bda64ba2aa84cb34
Bodhi14 <bodhi140802@gmail.com>
2022-05-20 21:55:57 +0530

    JS File of TEAM Webpage

--- team.js
+++ team.js
@@ -1,10 +1,6 @@
 let mybutton = document.getElementById("to-top");
 
 window.onscroll = function() {
-    scrollFunction();
-};
-
-function scrollFunction() {
     if (
         document.body.scrollTop > 10 ||
         document.documentElement.scrollTop > 10
@@ -13,10 +9,30 @@ function scrollFunction() {
     } else {
         mybutton.style.display = "none";
     }
-}
+};
+
 let act2 = document.getElementById("tact2");
 let act1 = document.getElementById("tact1");
-var flag = 0;
+
+if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    act2.classList.add("active");
+    act2.classList.remove("btn-secondary");
+    act2.classList.add("btn-primary");
+    act1.classList.remove("btn-primary");
+    act1.classList.add("btn-secondary");
+
+} else {
+    if (act1.classList.contains("active") == false) {
+        act1.classList.remove("btn-secondary");
+        act1.classList.add("btn-primary");
+        act2.classList.remove("active");
+        act2.classList.remove("btn-primary");
+        act2.classList.add("btn-secondary");
+
+        act1.classList.add("active");
+    }
+
+}
 
 function toscroll(x, y) {
 
@@ -36,6 +52,17 @@ function toscroll(x, y) {
 function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+    if (act1.classList.contains("active") == false) {
+        act1.classList.remove("btn-secondary");
+        act1.classList.add("btn-primary");
+        act2.classList.remove("active");
+        act2.classList.remove("btn-primary");
+        act2.classList.add("btn-secondary");
+
+        act1.classList.add("active");
+    }
+
+
 }
 
 function toactivate() {
